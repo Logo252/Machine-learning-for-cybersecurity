612ac6da5ba6bc8439321119ea307c24f7321ee5
Logo252 <arnasgrigaliunas8@gmail.com>
2017-01-03 08:49:27 +0200

    add new project remote repository

---
+++ src/parameters.py
@@ -0,0 +1,4 @@
+import os
+dir_name = os.path.dirname
+spam_file = os.path.join(dir_name(dir_name(__file__)), "datasets", "spam", "spambase.data.txt")
+phishing_file = os.path.join(dir_name(dir_name(__file__)), "datasets", "phishing", "training.phishing.data.txt")
