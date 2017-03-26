3af780a57919a9bae1eed97c8442955a08a87ae5
Logo252 <arnasgrigaliunas8@gmail.com>
2017-03-26 14:40:19 +0300

    modify parameters

--- src/parameters.py
+++ src/parameters.py
@@ -3,6 +3,5 @@ import os
 dir_name = os.path.dirname
 spam_file = os.path.join(dir_name(dir_name(__file__)), "datasets", "spam", "spambase.data.txt")
 phishing_file = os.path.join(dir_name(dir_name(__file__)), "datasets", "phishing", "training.phishing.data.txt")
-adware_file = os.path.join(dir_name(dir_name(__file__)), "datasets", "adware", "")
 trackware_file = os.path.join(dir_name(dir_name(__file__)), "datasets", "trackware", "")
 constructor_file = os.path.join(dir_name(dir_name(__file__)), "datasets", "constructor", "t")
