d8b6b5c5f71bbc37ab39170632ea27ef91587b87
Logo252 <arnasgrigaliunas8@gmail.com>
2017-04-30 20:47:39 +0300

    modify scripts

--- src/parameters.py
+++ src/parameters.py
@@ -4,18 +4,31 @@ import os
 
 dir_name = os.path.dirname
 
-# Training data for CS problems
+# Training data files for cybersecurity problems
 SPAM_FILE = os.path.join(dir_name(dir_name(__file__)), 'datasets', 'cs_problems', 'spam', 'training.spambase.data.txt')
 PHISHING_FILE = os.path.join(dir_name(dir_name(__file__)), 'datasets', 'cs_problems', 'phishing', 'training.phishing.data.txt')
 
-# Training data for New CS problems
+# Parameters for new cybersecurity problems
 
 # Trackware
 TRACKWARE_FILE = os.path.join(dir_name(dir_name(__file__)), 'datasets', 'new_cs_problems', 'trackware', 'training_trackware_data.txt')
 GENERATED_SAMPLES_FILE_FOR_TRACKWARE = os.path.join(dir_name(dir_name(__file__)), 'datasets', 'new_cs_problems', 'trackware', 'generated_samples.txt')
 NO_OF_TRACKWARE_FEATURES = 7
-
+LISTS_OF_TRACKWARE_SAMPLES = [[0, 1, 0, 0, 0, 1, 1, 1, 1],
+                              [0, 0, 0, 0, 0, 0, 0, 1, 1],
+                              [0, 0, 0, 0, 0, 1, 1, 1, 1],
+                              [1, 0, 0, 0, 0, 1, 1, 1, 1],
+                              [0, 0, 1, 1, 0, 1, 1, 1, 1],
+                              [0, 0, 0, 0, 1, 1, 1, 1, 1]
+                              ]
 # Constructor
 CONSTRUCTOR_FILE = os.path.join(dir_name(dir_name(__file__)), 'datasets', 'new_cs_problems', 'constructor', 'training_constructor_data.txt')
 GENERATED_SAMPLES_FIILE_FOR_CONSTRUCTOR = os.path.join(dir_name(dir_name(__file__)), 'datasets', 'new_cs_problems', 'constructor', 'generated_samples.txt')
 NO_OF_CONSTRUCTOR_FEATURES = 8
+LISTS_OF_CONSTRUCTOR_SAMPLES = [[0, 1, 0, 0, 0, 1, 1, 1, 1],
+                                [0, 0, 0, 0, 0, 0, 0, 1, 1],
+                                [0, 0, 0, 0, 0, 1, 1, 1, 1],
+                                [1, 0, 0, 0, 0, 1, 1, 1, 1],
+                                [0, 0, 1, 1, 0, 1, 1, 1, 1],
+                                [0, 0, 0, 0, 1, 1, 1, 1, 1]
+                                ]
--- src/synthetic_data_generator.py
+++ src/synthetic_data_generator.py
@@ -4,45 +4,78 @@ import random
 
 from src.parameters import GENERATED_SAMPLES_FIILE_FOR_CONSTRUCTOR
 from src.parameters import NO_OF_CONSTRUCTOR_FEATURES
+from src.parameters import LISTS_OF_CONSTRUCTOR_SAMPLES
 
 from src.parameters import GENERATED_SAMPLES_FILE_FOR_TRACKWARE
 from src.parameters import NO_OF_TRACKWARE_FEATURES
+from src.parameters import LISTS_OF_TRACKWARE_SAMPLES
 
 # Constants
-SAMPLES = 50
+NO_OF_SAMPLES = 50  # Number of generated samples for each problem
 
-# Possible value of the feature
-MINIMUM = 0
-MAXIMUM = 1
+# Possible values of the feature
+ZERO = 0
+ONE = 1
 
 
 def run_script():
     """
     Runs the script as the standalone program
     """
-    export_random_data(SAMPLES, MINIMUM, MAXIMUM, GENERATED_SAMPLES_FIILE_FOR_CONSTRUCTOR, NO_OF_CONSTRUCTOR_FEATURES)
-    print("Generated data has been exported to '{}' for constructor".format(GENERATED_SAMPLES_FIILE_FOR_CONSTRUCTOR))
+
+    # For constructor
+    file_name = GENERATED_SAMPLES_FIILE_FOR_CONSTRUCTOR
+    no_of_features = NO_OF_CONSTRUCTOR_FEATURES
+    data_samples = LISTS_OF_CONSTRUCTOR_SAMPLES
+
+    export_random_data(file_name, no_of_features, data_samples)
+    print("Generated random data has been exported to '{}' for constructor".format(file_name))
+
+    # For trackware
+    file_name = GENERATED_SAMPLES_FILE_FOR_TRACKWARE
+    no_of_features = NO_OF_TRACKWARE_FEATURES
+    data_samples = LISTS_OF_TRACKWARE_SAMPLES
+
+    export_random_data(file_name, no_of_features, data_samples)
+    print("Generated random data has been exported to '{}' for trackware".format(file_name))
 
 
-    export_random_data(SAMPLES, MINIMUM, MAXIMUM, GENERATED_SAMPLES_FILE_FOR_TRACKWARE, NO_OF_TRACKWARE_FEATURES)
-    print("Generated data has been exported to '{}' for trackware".format(GENERATED_SAMPLES_FILE_FOR_TRACKWARE))
-
-
-def export_random_data(samples, minimum, maximum, file_name, no_of_features):
+def export_random_data(file_name, no_of_features, data_samples):
     """
     Exports generated random data to specified file.
-    :param samples: samples
-    :param minimum: minimum value of the feature
-    :param maximum: maximum value of the feature
     :param file_name: file name
     :param no_of_features: number of features
+    :param data_samples: malicious data samples
     """
     with open(file_name, "w") as txt_file:
         i = 1
-        while i <= samples:
+        while i <= NO_OF_SAMPLES:
+
+            new_sample = []
             for _ in range(no_of_features):
-                txt_file.write(
-                    "{}, ".format(random.randint(minimum, maximum)))
+                feature_value = random.randint(ZERO, ONE)
+                new_sample.append(feature_value)
+
+                txt_file.write('{}, '.format(feature_value))
+
+            category = None
+            # new_sample = [1, 1, 0, 0, 0, 1, 1, 0]  # For debugging
+
+            # Finding out in which category new sample belongs to (comparing with given malicious data samples)
+            for data_sample in data_samples:
+                for index in range(len(new_sample)):
+                    if data_sample[index] == 1:
+                        # If malicious sample's feature value (1) matches to new sample's feature value
+                        # then set sample as malicious, otherwise as benign and break cycle
+                        if data_sample[index] == new_sample[index]:
+                            category = '1'
+                        else:
+                            category = '0'
+                            break
+                if category == '1':  # If new sample's category is 1 then do not compare further to others data samples
+                    break
+
+            txt_file.write(category)  # new category - 0 or 1
             txt_file.write('\n')
             i += 1
 
