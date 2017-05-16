ce06dc29db5cda98885b0e8c05f55b695d840d19
Arnas Grigaliunas <arnas.grigaliunas@visma.com>
2017-05-16 08:40:43 +0200

    some fixes

--- src/parameters.py
+++ src/parameters.py
@@ -9,7 +9,6 @@ TRAINING_DATA_FOR_SPAM = os.path.join(dir_name(dir_name(__file__)), 'datasets', 
 TRAINING_DATA_FOR_PHISHING = os.path.join(dir_name(dir_name(__file__)), 'datasets', 'cs_problems', 'phishing', 'training.phishing.data.txt')
 
 # Parameters for new cybersecurity problems
-
 # Constructor
 CONSTRUCTOR_SAMPLES_FILE = os.path.join(dir_name(dir_name(__file__)), 'datasets', 'new_cs_problems', 'constructor', 'constructor_samples.txt')
 ALL_SAMPLES_FOR_CONSTRUCTOR = os.path.join(dir_name(dir_name(__file__)), 'datasets', 'new_cs_problems', 'constructor', 'all_samples.txt')
--- src/synthetic_data_generator.py
+++ src/synthetic_data_generator.py
@@ -6,12 +6,15 @@ import numpy as np
 import pandas as pd
 from sklearn.feature_selection import VarianceThreshold
 
-from src.parameters import ALL_SAMPLES_FOR_CONSTRUCTOR
+# Trackware
 from src.parameters import ALL_SAMPLES_FOR_TRACKWARE
-from src.parameters import CONSTRUCTOR_SAMPLES_FILE
+from src.parameters import GENERATED_SAMPLES_FILE_FOR_TRACKWARE
 from src.parameters import TRACKWARE_SAMPLES_FILE
+
+# Constructor
+# from src.parameters import ALL_SAMPLES_FOR_CONSTRUCTOR
+# from src.parameters import CONSTRUCTOR_SAMPLES_FILE
 # from src.parameters import GENERATED_SAMPLES_FILE_FOR_CONSTRUCTOR
-# from src.parameters import GENERATED_SAMPLES_FILE_FOR_TRACKWARE
 
 # Constants
 NO_OF_SAMPLES = 100  # Number of samples which will be generated for the problem (duplicate samples will be removed)
@@ -28,27 +31,31 @@ def run_script():
     """
     Runs the script as the standalone program
     """
-    print('----------------------- CONSTRUCTOR -----------------------')
-    # generated_file_name = GENERATED_SAMPLES_FILE_FOR_CONSTRUCTOR
-    all_samples_file_name = ALL_SAMPLES_FOR_CONSTRUCTOR
-    data_samples_file = CONSTRUCTOR_SAMPLES_FILE
+    # print('----------------------- CONSTRUCTOR -----------------------')
+    # generated_samples_file_name = GENERATED_SAMPLES_FILE_FOR_CONSTRUCTOR
+    # all_samples_file_name = ALL_SAMPLES_FOR_CONSTRUCTOR
+    # data_samples_file = CONSTRUCTOR_SAMPLES_FILE
+    #
+    # data_frame = pd.read_csv(data_samples_file, sep=', ', engine='python')
+    #
+    # if MODE:
+    #     data_frame = remove_unneeded_features(data_frame)
+    # no_of_features = len(data_frame.columns)
+    #
+    # export_all_samples(all_samples_file_name, no_of_features,
+    #                    data_frame)
+    # print(
+    #     "All samples have been exported to\n'{}' for constructor\n".format(
+    #         all_samples_file_name))
+    # print(
+    #     "Generated samples have been exported to\n'{}' for trackware\n".format(
+    #         generated_samples_file_name))
 
-    data_frame = pd.read_csv(data_samples_file, sep=', ', engine='python')
-
-    if MODE:
-        data_frame = remove_unneeded_features(data_frame)
-    no_of_features = len(data_frame.columns)
-
-    export_all_samples(all_samples_file_name, no_of_features,
-                       data_frame)
-    print(
-        "Generated random data has been exported to\n'{}' for constructor\n".format(
-            all_samples_file_name))
 
     # sort_file_lines_by_category(file_name, no_of_features)  # from 0 (benign) to 1 (malware)
 
     print('----------------------- TRACKWARE -----------------------')
-    # generated_file_name = GENERATED_SAMPLES_FILE_FOR_TRACKWARE
+    generated_samples_file_name = GENERATED_SAMPLES_FILE_FOR_TRACKWARE
     all_samples_file_name = ALL_SAMPLES_FOR_TRACKWARE
     data_samples_file = TRACKWARE_SAMPLES_FILE
 
@@ -57,11 +64,18 @@ def run_script():
         data_frame = remove_unneeded_features(data_frame)
     no_of_features = len(data_frame.columns)
 
-    export_all_samples(all_samples_file_name, no_of_features,
-                       data_frame)
+    export_samples(all_samples_file_name,
+                   generated_samples_file_name,
+                   no_of_features,
+                   data_frame)
     print(
-        "Generated random data has been exported to\n'{}' for trackware\n".format(
+        "All samples have been exported to\n'{}'\n".format(
             all_samples_file_name))
+
+    print(
+        "Generated samples have been exported to\n'{}'\n".format(
+            generated_samples_file_name))
+
 
     # sort_file_lines_by_category(file_name, no_of_features)  # from 0 (benign) to 1 (malware)
 
@@ -81,39 +95,48 @@ def remove_unneeded_features(samples):
     return pd.DataFrame(selector.fit_transform(samples), columns=feature_names)
 
 
-def export_all_samples(all_samples_file_name, no_of_features,
-                       data_frame):
+def export_samples(all_samples_file_name,
+                   generated_samples_file_name,
+                   no_of_features,
+                   data_frame):
     """
-    Exports malware samples and generated samples to specified file.
+    Exports all samples and generated samples to specified files.
     :param all_samples_file_name: all samples file name
+    :param generated_samples_file_name: generated samples file name
     :param no_of_features: number of features
     :param data_frame: malicious data samples
     """
-    with open(all_samples_file_name, 'w') as txt_file:
+    with open(all_samples_file_name, 'w') as all_samples, \
+            open(generated_samples_file_name, 'w') as generated_samples:
 
         # Writing features to the file and to console
         features = list(data_frame)
         features_as_string = ', '.join(str(feature) for feature in features)
-        txt_file.write(features_as_string)
-        txt_file.write(', class')
-        txt_file.write('\n')  # new line
-        print("Features names:\n{}\n".format(features_as_string))
+
+        all_samples.write(features_as_string)
+        all_samples.write(', class')
+        all_samples.write('\n')  # new line
+        generated_samples.write(features_as_string)
+        generated_samples.write(', class')
+        generated_samples.write('\n')  # new line
+
+        # print("Features names:\n{}\n".format(features_as_string))
 
         # Samples from data frame as numpy array (without header)
-        samples = np.array(data_frame.as_matrix())
-        samples = samples.tolist()
+        malicious_samples = np.array(data_frame.as_matrix())
+        malicious_samples = malicious_samples.tolist()
 
         # If all features are 0 in sample then remove it from samples list
-        samples = [sample for sample in samples if
-                   not all(feature == 0 for feature in sample)]
+        malicious_samples = [sample for sample in malicious_samples if
+                             not all(feature == 0 for feature in sample)]
 
-        # Writing samples to the file
-        for sample in samples:
+        # Writing malicious samples to the file
+        for sample in malicious_samples:
             sample_as_string = ', '.join(str(feature) for feature in sample)
-            txt_file.write(sample_as_string)
-            txt_file.write(
+            all_samples.write(sample_as_string)
+            all_samples.write(
                 ', 1')  # category - 1 (malware) at the end of the line
-            txt_file.write('\n')  # new line
+            all_samples.write('\n')  # new line
 
         # Writing generated random samples to the file
         i = 1
@@ -126,20 +149,26 @@ def export_all_samples(all_samples_file_name, no_of_features,
                 feature_value = random.randint(ZERO, ONE)
                 new_sample.append(feature_value)
 
-            # If the new sample has been already generated or he is the same as real sample
+            # If the new sample has been already generated or he is the same as malicious sample
             # then skip everything and try to generate new sample
-            if new_sample in new_samples or new_sample in samples:
+            if new_sample in new_samples or new_sample in malicious_samples:
                 continue
             else:
                 new_samples.append(new_sample)
                 new_sample_as_string = ', '.join(
                     str(feature) for feature in new_sample)
-                txt_file.write(new_sample_as_string)
 
-            category = get_category(samples, no_of_features, new_sample)
+                all_samples.write(new_sample_as_string)
+                generated_samples.write(new_sample_as_string)
 
-            txt_file.write(', {}'.format(category))  # new category - 0 or 1
-            txt_file.write('\n')  # new line
+            category = get_category(malicious_samples, no_of_features,
+                                    new_sample)
+
+            all_samples.write(', {}'.format(category))  # new category - 0 or 1
+            all_samples.write('\n')  # new line
+
+            generated_samples.write(', {}'.format(category))  # new category - 0 or 1
+            generated_samples.write('\n')  # new line
 
 
 def get_category(samples, no_of_features, new_sample):
